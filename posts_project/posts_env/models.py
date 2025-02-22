896a6f1dfcc076f06fbb317aeef860681e117e97
Angelo Gondraneos <lr.agondraneos@mmdc.mcl.edu.ph>
2025-02-22 22:30:01 +0800

    Create resources

--- posts_project/posts_env/models.py
+++ posts_project/posts_env/models.py
@@ -18,3 +18,16 @@ class Post(models.Model):
     def __str__(self):
         return self.content[:50]
 
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.username
+
+
+class Post(models.Model):
+    content = models.TextField()
+
